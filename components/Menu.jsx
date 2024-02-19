528eacfdd84f0a2f05cd2792aa7583cb267618eb
abdulrafay0608 <abdulrafay0608@gmail.com>
2024-02-20 02:43:14 +0500

    first commit

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -12,8 +12,8 @@ const data = [
 const subMenuData = [
     { id: 1, name: "Jordan", doc_count: 11 },
     { id: 2, name: "Sneakers", doc_count: 8 },
-    { id: 3, name: "Running shoes", doc_count: 64 },
-    { id: 4, name: "Football shoes", doc_count: 107 },
+    { id: 3, name: "Running-shoes", doc_count: 64 },
+    { id: 4, name: "Football-shoes", doc_count: 107 },
 ];
 
 const Menu = ({ showCatMenu, setShowCatMenu, categories }) => {
